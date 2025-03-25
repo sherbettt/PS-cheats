14f80be4496702b27095696de5b1e0da35633a9c
Kirill K <44464893+sherbettt@users.noreply.github.com>
2025-03-25 17:04:15 +0300

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+### file should be kept in c:\Users\kiko0217\Documents\PowerShell\
+
+##oh-my-posh themes
+# to watch all themes: Get-PoshThemes
+# to use theme in console:
+# & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" --print) -join "`n"))
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\clean-detailed.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" | Invoke-Expression
+
+
+## Alias
+New-Alias -Name gh -Value Get-Help
+function ll {ls -Hidden && ls}
+function wupall {winget upgrade --all --include-unknown}
